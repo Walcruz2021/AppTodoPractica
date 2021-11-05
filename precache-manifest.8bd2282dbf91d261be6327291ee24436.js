c227d4d8e4cb9f91d80ec6a4abdf947f184178c1
Walcruz2021 <cruzherreria1@gmail.com>
2021-11-05 13:28:04 -0300

    Updates

---
+++ precache-manifest.8bd2282dbf91d261be6327291ee24436.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18fa6513821646ed1017f5f71eeb3753",
+    "url": "/AppTodoPractica/index.html"
+  },
+  {
+    "revision": "1c3706c85a108baabc99",
+    "url": "/AppTodoPractica/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7ad0e279b3f5615a5b70",
+    "url": "/AppTodoPractica/static/css/main.9a856e95.chunk.css"
+  },
+  {
+    "revision": "1c3706c85a108baabc99",
+    "url": "/AppTodoPractica/static/js/2.18e79ec3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/AppTodoPractica/static/js/2.18e79ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad0e279b3f5615a5b70",
+    "url": "/AppTodoPractica/static/js/main.9af14e24.chunk.js"
+  },
+  {
+    "revision": "73b84cc25c9b0f81023a",
+    "url": "/AppTodoPractica/static/js/runtime-main.92d420ce.js"
+  }
+]);
